74f8a2ff8d7decfe31b0c7f8bfc2b17c8a09be0d
kiousis <kiousis@gmail.com>
2018-01-21 19:57:11 +0200

    Settings for logging

--- src/settings.php
+++ src/settings.php
@@ -1,4 +1,19 @@
 <?php
 /**
  * The settings file
- * */
+ */
+
+
+return [
+    'settings' => [
+        'displayErrorDetails' => true, // set to false in production
+        'addContentLengthHeader' => false, // Allow the web server to send the content-length header
+
+        // Monolog settings
+        'logger' => [
+            'name' => 'slim-app',
+            'path' =>  __DIR__ . '/../logs/app.log',
+            'level' => \Monolog\Logger::DEBUG,
+        ],
+    ],
+];
