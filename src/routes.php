a026b7a51bc240dae7ef5b8c750dab52dc0efa61
kiousis <kiousis@gmail.com>
2018-01-21 20:05:03 +0200

    logger tests

--- src/routes.php
+++ src/routes.php
@@ -7,12 +7,14 @@ use Slim\Http\Response;
 
 
 $app->get('/', function (Request $request, Response $response, array $args) {
+    $this->logger->info("travel-guide api '/' route");
     $response->getBody()->write("api home");
 
     return $response;
 });
 
 $app->get('/version', function (Request $request, Response $response, array $args) {
+    $this->logger->debug("travel-guide api '/version' route");
     $response->getBody()->write("api version 0.1");
 
     return $response;
