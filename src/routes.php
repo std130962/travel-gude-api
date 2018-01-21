2e70567cbbcbf67f02dadc9f34bfa61eb6555584
kiousis <kiousis@gmail.com>
2018-01-21 19:51:44 +0200

    Demo routes

--- src/routes.php
+++ src/routes.php
@@ -2,3 +2,18 @@
 /**
  * The routes of the api
  */
+use Slim\Http\Request;
+use Slim\Http\Response;
+
+
+$app->get('/', function (Request $request, Response $response, array $args) {
+    $response->getBody()->write("api home");
+
+    return $response;
+});
+
+$app->get('/version', function (Request $request, Response $response, array $args) {
+    $response->getBody()->write("api version 0.1");
+
+    return $response;
+});
