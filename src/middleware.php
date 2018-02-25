88f9c6872de40bf1d98987b7c932500f3825915f
kiousis <kiousis@gmail.com>
2018-02-25 21:42:35 +0200

    Middleware

--- src/middleware.php
+++ src/middleware.php
@@ -2,3 +2,25 @@
 /**
  * The middlaware file
  */
+
+// Parameters middlware
+$pmw = function ($request, $response, $next) {
+
+    // Get all parameters
+    $parameters = $request->getQueryParams();
+    $this->logger->debug("parameters: ", $parameters);
+
+    $params = array();
+    $params['limit'] = (int) helper::setLimit($parameters);
+    $params['offset'] = (int) helper::setOffset($parameters);
+    $params['full'] = (int) helper::setFull($parameters);
+
+    $this->logger->debug("params: ", $params);
+
+    $request = $request->withAttribute('params', $params);
+
+    $response = $next($request, $response);
+    //$response->getBody()->write('AFTER');
+
+    return $response;
+};
